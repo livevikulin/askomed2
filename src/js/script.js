314458612b189478d3bebec951cafdf2128094d3
Виталий Викулин <livevikulin@gmail.com>
2019-08-11 19:13:36 +0700

    Сверстал шапку, футер, главную и карточку с товаром + адаптив

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+import "jquery";
+import "jquery-ui-bundle";
+
+$(document).ready(function() {
+
+	$('select').selectmenu();
+	$('.ui-selectmenu-button').on('click', function() {
+		$('.ui-selectmenu-icon').toggleClass('arrow');
+	});
+	$('.ui-selectmenu-menu').on('click', function() {
+		$('.ui-selectmenu-icon').removeClass('arrow');
+	});
+
+	$('#search').on('change input', function() {
+		if ($(this).val() == "") {
+			$(this).parent().find('.search-close').hide({
+				duration: 200,
+			});
+		} else if ($(this).val() != "") {
+			$(this).parent().find('.search-close').show({
+				duration: 200,
+			});
+		}
+	})
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
 	output: {
 		filename: "[name].js"
 	},
-
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+			'window.$': 'jquery'
+		}),
+	],
 	module: {
 		rules: [
 			{
