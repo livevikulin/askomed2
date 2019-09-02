a0bd4d97d1f7c4dfc9fbc37f5693ae7c2324f39a
Виталя Викулин <livevikulin@gmail.com>
2019-09-02 18:17:08 +0700

    залил папку dist

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import "jquery";
 import "jquery-ui-bundle";
+import "jquery.maskedinput";
 
 $(document).ready(function() {
 
@@ -23,5 +24,55 @@ $(document).ready(function() {
 			});
 			$('.search__result').show();
 		}
-	})
+	});
+	
+	var $input = $('input');
+	$input.on('change', function() {
+		if ($(this).val() == "") {
+			$(this).addClass('error');
+			$(this).parent('.input-card').removeClass('succsess-icon');
+			$(this).parent('.input-card').addClass('error-icon');
+			$(this).parent('.input-month').addClass('error-icon');
+			$(this).parent('.input-month').removeClass('succsess-icon');
+			$(this).parent('.input-security').addClass('error-icon');
+			$(this).parent('.input-security').removeClass('succsess-icon');
+			$(this).removeClass('succsess');
+		} else if ($(this).val() != "") {
+			$(this).removeClass('error');
+			$(this).parent('.input-card').removeClass('error-icon');
+			$(this).parent('.input-card').addClass('succsess-icon');
+			$(this).parent('.input-month').removeClass('error-icon');
+			$(this).parent('.input-month').addClass('succsess-icon');
+			$(this).parent('.input-security').removeClass('error-icon');
+			$(this).parent('.input-security').addClass('succsess-icon');
+			$(this).addClass('succsess');
+		}
+	});
+	
+	$('.pay-btn').on('click', function(e) {
+		e.preventDefault();
+		$('.modals').css({
+			display: "block"
+		});
+		$('.modal').addClass('modals-active');
+	});
+	$('.modal__close').on('click', function(e) {
+		e.preventDefault();
+		$('.modals').css({
+			display: "none"
+		});
+		$(this).parent().removeClass('modals-active');
+	});
+	$('#modals').on('change input', function() {
+		var active = $('#modals');
+		if (active.val() == "") {
+			$('.modal__btn').addClass('disabled-btn')
+		} else if(active.val() != "") {
+			$('.disabled-btn').removeClass('disabled-btn')
+		}
+	});
+	
+	$("#bankCard").mask("9999 9999 9999 9999");
+	$("#cardMonth").mask("99/99");
+	$("#securityCard").mask("999");
 })
